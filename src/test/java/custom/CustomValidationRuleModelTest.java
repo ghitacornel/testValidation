6ab7e6617ba127d955f08e9ccccdaf2accff1272
cornel <ghita.cornel@gmail.com>
2020-10-08 09:47:02 +0300

    better tests

--- src/test/java/custom/CustomValidationRuleModelTest.java
+++ src/test/java/custom/CustomValidationRuleModelTest.java
@@ -29,7 +29,7 @@ public class CustomValidationRuleModelTest extends Setup {
         Set<ConstraintViolation<CustomValidationRuleModel>> violations = validator.validate(subject);
         Assert.assertEquals(1, violations.size());
         ConstraintViolation<CustomValidationRuleModel> violation = violations.iterator().next();
-        Assert.assertEquals("bad order, accepted 'top order' or 'low order'", violation.getMessage());
+        Assert.assertEquals("illegal order value 'xxx', accepted values : 'top order' or 'low order'", violation.getMessage());
         Assert.assertEquals("{my.custom.validation.rule.OrderType.message}", violation.getMessageTemplate());
         Assert.assertEquals("orderType", violation.getPropertyPath().toString());
     }
