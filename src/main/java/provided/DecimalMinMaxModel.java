5713053545e9b83768fe7105a494098f99c9fcf1
cornel <ghita.cornel@gmail.com>
2022-03-10 22:47:05 +0200

    better

--- src/main/java/provided/DecimalMinMaxModel.java
+++ src/main/java/provided/DecimalMinMaxModel.java
@@ -2,8 +2,6 @@ package provided;
 
 import javax.validation.constraints.DecimalMax;
 import javax.validation.constraints.DecimalMin;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import java.math.BigDecimal;
 
 public class DecimalMinMaxModel {
--- src/test/java/setup/Setup.java
+++ src/test/java/setup/Setup.java
@@ -1,8 +1,5 @@
 package setup;
 
-import org.junit.Before;
-import org.junit.BeforeClass;
-
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
@@ -10,26 +7,9 @@ import javax.validation.ValidatorFactory;
 public class Setup {
 
     // usually a SINGLETON
-    private static ValidatorFactory factory;
+    private static final ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
 
-    // usually a PROTOTYPE or ThreadLocal
-    protected Validator validator;
-
-    @BeforeClass
-    static public void beforeAll() {
-        factory = Validation.buildDefaultValidatorFactory();
-    }
-
-    /**
-     * HAD TO RECREATE THE VALIDATOR BEFORE EACH TEST<br>
-     * LOOKS LIKE THIS VALIDATOR IS NOT SO THREAD SAFE
-     */
-    @Before
-    public void before() {
-
-        // should be thread safe
-        validator = factory.getValidator();
-
-    }
+    // thread safe
+    protected final Validator validator = factory.getValidator();
 
 }
